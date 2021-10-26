36f738bbe1af2da7db4c9288a1b11f2937f7512f
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-10-26 16:14:07 +0200

    added input-fetch logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      app
-    </div>
-  )
-}
+  const [number, setNumber] = useState(0);
+  const [delay, setDelay] = useState(0);
 
-export default App
+ 
+
+  const handleFetch = () => {
+    fetch("https://random-data-api.com/api/stripe/random_stripe").then(
+      (response) => {
+        response.json()
+        .then((data) => console.log(data));
+      }
+    );
+  }
+
+  const handleStart = () => {
+    for(let i = 0; i < number; i++){
+      setTimeout(handleFetch, (delay * 1000) * i)
+    } 
+  }
+ 
+
+  return (
+    <div className="App">
+      <div className="pair">
+        <label htmlFor="number">Enter Number of fetch requests: </label>
+        <input
+          type="number"
+          id="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+      <div className="pair">
+        <label htmlFor="times">How long between each request (s): </label>
+        <input
+          type="number"
+          id="times"
+          value={delay}
+          onChange={(e) => setDelay(e.target.value)}
+        />
+      </div>
+      <button onClick={handleStart}>Start</button>
+    </div>
+  );
+};
+
+export default App;
