50e34b51d75e406f537f0e30ee5042065acf9a96
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-10-26 19:37:26 +0200

    logic done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 
 const App = () => {
   const [number, setNumber] = useState(0);
   const [delay, setDelay] = useState(0);
-
- 
+  const [times, setTimes] = useState([])
+  const [average, setAverage] = useState(0)
 
   const handleFetch = () => {
+    const start = performance.now();
     fetch("https://random-data-api.com/api/stripe/random_stripe").then(
       (response) => {
-        response.json()
-        .then((data) => console.log(data));
+        response.json().then((data) => console.log(data));
       }
     );
-  }
+    const end = performance.now();
+    const elapsedTime = end - start;
+    setTimes(prev => [...prev, elapsedTime])
+  };
 
   const handleStart = () => {
-    for(let i = 0; i < number; i++){
-      setTimeout(handleFetch, (delay * 1000) * i)
-    } 
-  }
- 
+    setTimes([])
+    for (let i = 0; i < number; i++) {
+      setTimeout(handleFetch, delay * 1000 * i);
+    }
+  };
+
+  useEffect(()=>{
+    let sum =times.reduce((a, b) => a + b, 0 ).toFixed(2)
+    console.log(sum)
+    setAverage(sum / times.length)
+  }, [setAverage, times])
+
+
 
   return (
     <div className="App">
@@ -34,7 +46,7 @@ const App = () => {
         />
       </div>
       <div className="pair">
-        <label htmlFor="times">How long between each request (s): </label>
+        <label htmlFor="times">How long between each request (in seconds): </label>
         <input
           type="number"
           id="times"
@@ -42,6 +54,7 @@ const App = () => {
           onChange={(e) => setDelay(e.target.value)}
         />
       </div>
+      <h2>Average time: {average.toFixed(2)}s</h2>
       <button onClick={handleStart}>Start</button>
     </div>
   );
